bdd757e08497e31e0776b3a7d4a22417b0441f4b
Adam Disuky <adam.disuky@hotmail.com>
2018-11-15 23:23:51 -0500

    Added endpoint for grabbing employees schedule by eid

---
+++ project/api/v1/schedule/schedule_controller.php
@@ -0,0 +1,50 @@
+<?php
+include_once '../../database/database.php';
+include_once '../../utils/helpers.php';
+
+/**
+ * Check if the user can login
+ * @return JSON login info
+ */
+
+function get_schedule($eid)
+{
+
+    try {
+        $database = new Database();
+        $db = $database->getConnection();
+
+        if (!test_db_connection($db)) {
+            return array("error" => "Cannot connect to DB.");
+        }
+
+        $query = "SELECT * FROM SCHEDULE WHERE eid = '".$eid."'";
+
+        $packet = array();
+
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+
+        $number_of_rows = $stmt->fetchColumn();
+        $stmt->execute();
+        if ($number_of_rows > 0) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $tmp = array(
+                    'employeeExists' => True,
+                    'eid' => $row['eid'],
+                    'startTime' => $row['startTime'],
+                    'endTime' => $row['endTime'],
+                    'isHoliday' => $row['isHoliday'],
+                    'isSickDay' => $row['isSickDay']
+                );
+                array_push($packet,$tmp);
+            } 
+            return $packet;
+        } else{
+            return array("employeeExists" => False);
+        }
+    } catch (Exception $e){
+        return array("error" => "Server error ".$e." .");
+    }
+}
+
