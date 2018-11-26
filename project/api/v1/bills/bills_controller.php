340f07c5c74b514bc25e9d3cf891e273f8e455c5
Adam Disuky <adam.disuky@hotmail.com>
2018-11-25 23:13:19 -0500

    Added getting bills from accountNumber

---
+++ project/api/v1/bills/bills_controller.php
@@ -0,0 +1,51 @@
+<?php
+include_once '../../database/database.php';
+include_once '../../utils/helpers.php';
+
+/**
+ * Check if the user can login
+ * @return JSON login info
+ */
+
+
+function pay_bills($data)
+{
+
+    try {
+        $database = new Database();
+        $db = $database->getConnection();
+    
+        if (!test_db_connection($db)) {
+            return array("error" => "Cannot connect to DB.");
+        }
+    $packet = array();
+    $accNum = $data['accountNumber'];
+    $query = "SELECT MyPayee.id FROM MyPayee INNER JOIN Account ON MyPayee.accountNumber = Account.accountNumber WHERE MyPayee.accountNumber = ".$accNum." AND Account.accountNumber = ".$accNum.";";
+    $stmt = $db->prepare($query); 
+    $stmt->execute();
+
+    $number_of_rows = $stmt->fetchColumn();
+    $stmt->execute();
+
+    if ($number_of_rows > 0) {
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $id = $row['id'];
+            $query1 = "SELECT * FROM MyPayee INNER JOIN Bills ON MyPayee.id = Bills.MyPayeeId WHERE MyPayee.id = ".$id." AND Bills.MyPayeeId = ".$id.";";
+            $stmt1 = $db->prepare($query1);
+            $stmt1->execute();
+            $number_of_rows1 = $stmt1->fetchColumn();
+            $stmt1->execute();
+            while ($row1 = $stmt1->fetch(PDO::FETCH_ASSOC)) {
+                array_push($packet, $row1);
+                }
+            }
+        return $packet;
+    } else {
+            return array("billExists" => False);
+    }
+
+    } catch (Exception $e) {
+        return array("error" => "Server error ".$e." .");
+    }
+}
+
