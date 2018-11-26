f57e7b5db23d71406ebe66e008cf104ed00e2e2c
Benjamin Therien <benjamintherien@yahoo.ca>
2018-11-26 14:50:55 -0500

    added revenue endpoint

--- project/api/v1/revenue/index.php
+++ project/api/v1/revenue/index.php
@@ -5,12 +5,13 @@
 header('Access-Control-Allow-Origin:  *');
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-require_once 'delemployee_controller.php';
+require_once 'revenue_controller.php';
 
 $http_method = $_SERVER['REQUEST_METHOD'];
 
 switch ($http_method) {
     case 'GET':
+    echo json_encode(get_revenue_branch());
         return;
     case 'POST':
         return;
